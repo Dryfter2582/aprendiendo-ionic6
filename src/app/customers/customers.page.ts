2bec77f203295536e406517b7bcfac9eb036bd9d
Dryfter2582 <anthonylopez2582@gmail.com>
2021-08-28 02:17:44 -0400

    subiendo primeros archivos

---
+++ src/app/customers/customers.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.page.html',
+  styleUrls: ['./customers.page.scss'],
+})
+export class CustomersPage implements OnInit {
+
+  users: any = [];
+
+  constructor(
+    private router: Router,
+    private http: HttpClient) { }
+
+  ngOnInit() {
+    console.log("hoola");
+    this.getUsers().subscribe(res=>{
+      console.log("Res",res)
+      this.users = res;
+    });
+  }
+
+  goToHome() {
+    this.router.navigate(['/home'])
+
+  }
+
+  getUsers(){
+    return this.http
+    .get("assets/files/customers.json")
+    .pipe(
+      map((res:any) =>{
+        return res.data;
+      })
+    )
+  }
+
+
+}
